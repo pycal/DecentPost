7032983fc95861865d599d5c1a840855a42e0ec1
Joshua Kelly <jdjkelly@gmail.com>
2018-02-17 11:54:08 -0700

    Basic scaffolding

---
+++ src/components/Deliver.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Deliver extends Component {
+  render() {
+    return (
+      <div>
+        deliver
+      </div>
+    )
+  }
+}
+
+export default Deliver;
