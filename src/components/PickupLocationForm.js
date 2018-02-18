3111ed0e683fc37c4d33b26867662ff7d68d7b25
Cailen McQuattie <cailen.mcquattie@n-dimension.com>
2018-02-18 00:55:00 -0500

    pickup working
    Signed-off-by: Cailen McQuattie <cailen.mcquattie@n-dimension.com>

--- src/components/PickupLocationForm.js
+++ src/components/PickupLocationForm.js
@@ -7,14 +7,15 @@ import Checkbox from 'material-ui/Checkbox'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
+import GoogleMapReact from 'google-map-react'
 
 const validate = values => {
   const errors = {}
   const requiredFields = [
-    'streetAddress',
-    'city',
-    'province',
-    'postalCode',
+    'pickupStreetAddress',
+    'pickupCity',
+    'pickupProvince',
+    'pickupPostalCode',
   ]
   requiredFields.forEach(field => {
     if (!values[field]) {
@@ -74,70 +75,80 @@ const renderSelectField = ({
   />
 )
 
-
-
 class PickupLocationForm extends Component {
   constructor(props) {
     super(props);
     this.setPickupLocation = this.setPickupLocation.bind(this);
+    // this.gmaps = {lat: 59.95, lng: 30.33, zoom: 11, api_key: 'AIzaSyAkLUTgsp7Bm2FO_o6hJholvCVM6jgtA3w'}.bind(this);
+    this.state = {center: {lat: 59.95, lng: 30.33}, zoom: 12, maps_api_key: 'AIzaSyBW10pqKiQRHZ3I3v3Uof1WpWextat-P_8'};
   }
+
   setPickupLocation(event) {
     let base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
     let pickup_address = this.props.fullAddress;
     let api_key = '&key=AIzaSyAkLUTgsp7Bm2FO_o6hJholvCVM6jgtA3w';
     var fetch_url = base_url + pickup_address + api_key
 
-    alert(fetch_url)
     fetch(fetch_url)
       .then(res => res.json())
       .then((result) => {
-        alert(JSON.stringify(result.results[0].geometry.location))
+        debugger
+        this.setState({
+          center: result.results[0].geometry.location
+        })
       })
   }
 
-
   render() {
+    debugger
     return (
-      <form>
-        <div >
-          <Field name="streetAddress" component={renderTextField} label="Street Address" fullWidth={true} />
-        </div>
-        <div>
-          <Field name="city" component={renderTextField} label="City" fullWidth={true} />
-        </div>
-        <div>
-          <Field name="province" component={renderTextField} label="Province" fullWidth={true} />
-        </div>
-        <div>
-          <Field name="postalCode" component={renderTextField} label="Postal Code" fullWidth={true} />
-        </div>
-        <div>
-          <Field
-            name="country"
-            component={renderSelectField}
-            label="Country"
-            fullWidth={true}
-          >
-            <MenuItem value="canada" primaryText="Canada" />
-            <MenuItem value="usa" primaryText="United States of America" />
-          </Field>
-        </div>
+      <div>
+        <form>
+          <div >
+            <Field name="pickupStreetAddress" component={renderTextField} label="Street Address" fullWidth={true} />
+          </div>
+          <div>
+            <Field name="pickupCity" component={renderTextField} label="City" fullWidth={true} />
+          </div>
+          <div>
+            <Field name="pickupProvince" component={renderTextField} label="Province" fullWidth={true} />
+          </div>
+          <div>
+            <Field name="pickupPostalCode" component={renderTextField} label="Postal Code" fullWidth={true} />
+          </div>
+          <div>
+            <Field
+              name="pickupCountry"
+              component={renderSelectField}
+              label="Country"
+              fullWidth={true}
+            >
+              <MenuItem value="canada" primaryText="Canada" />
+              <MenuItem value="usa" primaryText="United States of America" />
+            </Field>
+          </div>
 
-        <div>
-          <Field
-            name="notes"
-            component={renderTextField}
-            label="Notes - i.e. buzz 444 to gain access to building"
-            multiLine={true}
-            rows={2}
-            fullWidth={true}
-          />
-        </div>
-        <div id="map"></div>
-        <div>
-          <RaisedButton label='Set Pickup Location' onClick={this.setPickupLocation} />
-        </div>
-      </form>
+          <div>
+            <Field
+              name="pickupNotes"
+              component={renderTextField}
+              label="Notes - i.e. buzz 444 to gain access to building"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+              onClick={this.setPickupLocation}
+            />
+          </div>
+          <div style={{height:'200px', width:'100%',overflow:'hidden'}}>
+            <GoogleMapReact
+              bootstrapURLKeys={{key: this.state.maps_api_key}}
+              center={this.state.center}
+              defaultZoom={this.state.zoom}
+              style={{height:'200px',position:'relative'}}
+            />
+          </div>
+        </form>
+      </div>
     )
   }
 }
@@ -149,10 +160,9 @@ PickupLocationForm = reduxForm({
 
 const selector = formValueSelector('PickupLocationForm') // <-- same as form name
 PickupLocationForm = connect(state => {
-  // 1600+Amphitheatre+Parkway,+Mountain+View,+CA
-  const { streetAddress, city, province, postalCode } = selector(state, 'streetAddress', 'city', 'province', 'postalCode')
+  const { pickupStreetAddress, pickupCity, pickupProvince, pickupPostalCode } = selector(state, 'pickupStreetAddress', 'pickupCity', 'pickupProvince', 'pickupPostalCode')
   return {
-    fullAddress: `${streetAddress || ''}+${city || ''}+${province || ''}+${postalCode || ''}`
+    fullAddress: `${pickupStreetAddress || ''}+${pickupCity || ''}+${pickupProvince || ''}+${pickupPostalCode || ''}`
   }
 })(PickupLocationForm)
 
